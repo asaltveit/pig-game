98a83750140d57218c31cde4e688df267eb5cd37
Anna <annasaltveit@gmail.com>
2024-05-21 12:17:56 -0700

    Add a basic snatcher

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -5,6 +5,7 @@ export class Game extends Scene
     
     platforms: Phaser.Physics.Arcade.StaticGroup;
     player: Phaser.Physics.Arcade.Sprite;
+    snatcher: Phaser.Physics.Arcade.Sprite;
     cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
     //camera: Phaser.Cameras.Scene2D.Camera;
     //background: Phaser.GameObjects.Image;
@@ -14,6 +15,10 @@ export class Game extends Scene
     {
         super('Game');
     }
+
+    sendSnatcher () {
+        this.snatcher.setVelocityX(40);
+    } 
 
     create ()
     {
@@ -25,12 +30,22 @@ export class Game extends Scene
         // Pig player starts on right side of screen
         this.player = this.physics.add.sprite(900, 600, 'dude').setTintFill(0xFFC0CB);
         this.player.setBounce(0.2);
-
         // Pig player can't walk off the screen
         this.player.setCollideWorldBounds(true);
 
+         // Snatcher starts on left side of screen
+       this.snatcher = this.physics.add.sprite(0, 600, 'dude').setTintFill(0x000000)//.setDisplaySize(10, 200);
+       this.snatcher.setCollideWorldBounds(true);
+
         // Pig sits on the ground
         this.physics.add.collider(this.player, this.platforms);
+        // Snatcher doesn't fall through the floor
+        this.physics.add.collider(this.snatcher, this.platforms);
+       // Snatcher stops when it collides with pig
+       this.physics.add.collider(this.snatcher, this.player);
+       // Snatcher waits, and then moves towards the right
+       this.time.delayedCall(2000, this.sendSnatcher, undefined, this);
+
     }
 
     update() {
